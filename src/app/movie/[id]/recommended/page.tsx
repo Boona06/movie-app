368704f90c3a927cbd4e11fed31902e057118b5b
Boona06 <bb851913@gmail.com>
2025-03-27 21:32:06 +0800

    80 huvi

--- src/app/movie/[id]/recommended/page.tsx
+++ src/app/movie/[id]/recommended/page.tsx
@@ -1,11 +1,39 @@
 
 import Section from "@/app/components/section";
 
+type Props = {
+  params: {
+    id: string; // `string` байх ёстой
+  };
+};
 
-export default function Recommend({params} : {params : { id : string}}){
-    return(
-        <div>
-            <Section endpoint={`/movie/${params.id}/recommendations`} title="More like this"/>
-        </div>
-    );
+export default async function MovieDetailPage({ params }: Props) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzk2OTBmOTgzMGNlODA0Yjc4OTRhYzFkZWY0ZjdlOSIsIm5iZiI6MTczNDk0OTM3MS43NDIsInN1YiI6IjY3NjkzOWZiYzdmMTcyMDVkMTBiMGIxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2r2TerxSJdZGmGVSLVDkk6nHT0NPqY4rOcxHtMNt0aE ", 
+    },
+  };
+
+  const movieDetailResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/${params.id}/credits`,
+    options
+  );
+  const credits = await movieDetailResponse.json();
+
+  const movieResponse = await fetch(
+    `https://api.themoviedb.org/3/movie/${params.id}?language=en-US`,
+    options
+  );
+  const data = await movieResponse.json();
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.overview}</p>
+      <Section title="More Like This" endpoint={`/movie/${params.id}/recommendations`} />
+    </div>
+  );
 }
